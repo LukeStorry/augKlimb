fe94359d34712a0ad88a038e4c399b1d5c7cc8c1
Luke Storry <luke@lukestorry.co.uk>
2019-03-08 22:01:48 +0000

    added timer to accelerometer recorder

---
+++ src/Assets/Scripts/AccelerometerRecorder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccelerometerRecorder : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime = 0;
+
+    // Use this for initialization
+    void Start () {
+        timerText.text = "ready";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (startTime != 0)
+        {
+            timerText.text = (Time.time-startTime).ToString("#.00");
+        }
+    }
+
+    public void StartTimer()
+    {
+        startTime = Time.time;
+    }
+
+    public void StopTimer()
+    {
+        startTime = 0;
+    }
+
+}
