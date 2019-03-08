0021b6f593c95f5913ccb6d5fb55e6e88db48679
Luke Storry <luke@lukestorry.co.uk>
2019-03-08 18:55:46 +0000

    Added Version printing to app

---
+++ src/Assets/Scripts/Version.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour {
+    void Start()
+    {
+        GetComponent<Text>().text += Application.version;
+    }
+}
