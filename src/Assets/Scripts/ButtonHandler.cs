93689b957e868776f8030835cafb3b8dfe39b805
Luke Storry <luke@lukestorry.co.uk>
2019-03-08 18:13:05 +0000

    buttons working

---
+++ src/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour {
+
+	// Moves to the 
+	public void GoToScene (string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Moves to the video scene
+    public void OpenLink (string url) {
+        Application.OpenURL(url);
+    }
+}
