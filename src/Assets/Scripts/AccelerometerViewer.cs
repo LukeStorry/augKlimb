4f87b67218c19949441484a9499e544bfc41ab54
Luke Storry <luke@lukestorry.co.uk>
2019-03-12 22:18:58 +0000

    started work on data scrollbar

--- src/Assets/Scripts/AccelerometerViewer.cs
+++ src/Assets/Scripts/AccelerometerViewer.cs
@@ -9,6 +9,10 @@ using UnityEngine.UI;
 public class AccelerometerViewer : MonoBehaviour
 {
     public Text fileListTextBox;
+    public GameObject scrollContent;
+    public GameObject dataItem;
+    public int dataItemHeight = 200;
+    public int padding = 5;
 
     void Start () {
         string filenames = "";
@@ -17,6 +21,11 @@ public class AccelerometerViewer : MonoBehaviour
             filenames += file.Name + "\n";
         }
         fileListTextBox.text = filenames;
+
+        GameObject one = Instantiate(dataItem, new Vector3(100, 100, 1), Quaternion.identity, scrollContent.transform);
+
+        GameObject two = Instantiate(dataItem, new Vector3(0, 0, 0), Quaternion.identity, scrollContent.transform);
+
     }
 
 
