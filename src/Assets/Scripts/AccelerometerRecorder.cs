7f8ad81f0c11497ea45ba0264da0980635c78f56
Luke Storry <luke@lukestorry.co.uk>
2019-03-12 15:11:30 +0000

    Added Timestamps to acc-recording, moved to use fixedupdate
    Looked into time variation - also saved timestamp to csv, accelerometer data coming in varies in time, from 0.006s to 0.52s
    Especially issue when phone detected large acceleration, wanted to chane orientation, which slows down update.
    Removed auto-orient,  moved to using fixedupdate and set timestep to 0.05 => 20 samples per second

--- src/Assets/Scripts/AccelerometerRecorder.cs
+++ src/Assets/Scripts/AccelerometerRecorder.cs
@@ -9,10 +9,11 @@ public class AccelerometerRecorder : MonoBehaviour
     public Text timerText;
     public Button startButton, stopButton;
 
-    private float startTime = -1;
+    private Int64 startTime;
+    private List<Int64> times;
     private List<float> accs;
     private bool running = false;
-    public Color buttonReadyColour, buttonNotReadyColour;
+    private Color buttonReadyColour, buttonNotReadyColour;
 
     void Start()
     {
@@ -23,12 +24,13 @@ public class AccelerometerRecorder : MonoBehaviour
         buttonNotReadyColour = stopButton.GetComponent<Image>().color;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (running)
         {
             accs.Add(new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z).magnitude);
-            timerText.text = (Time.time - startTime).ToString("#0.00");
+            times.Add(DateTime.Now.Ticks);
+            timerText.text = "Time: " + ((DateTime.Now.Ticks - startTime) / 10000000.0).ToString("#0.00");
         }
     }
 
@@ -39,7 +41,8 @@ public class AccelerometerRecorder : MonoBehaviour
         running = true;
         Debug.Log("Started");
 
-        startTime = Time.time;
+        startTime = DateTime.Now.Ticks;
+        times = new List<Int64>();
         accs = new List<float>();
         startButton.GetComponent<Image>().color = buttonNotReadyColour;
         stopButton.GetComponent<Image>().color = buttonReadyColour;
@@ -50,13 +53,22 @@ public class AccelerometerRecorder : MonoBehaviour
         if (!running) return;
         running = false;
         Debug.Log("Stopped");
-        timerText.text = "Time: " + (Time.time - startTime).ToString("#0.00")
-            + "\nSmoothness: " + CalcSmoothness().ToString("#0.00");
 
-        string csvString = "";
-        foreach (float acc in accs)
+        timerText.text += "\nTotal Deviance from Mean: " + CalcSmoothness(accs).ToString("#0.00");
+
+        SaveLists();
+
+        startButton.GetComponent<Image>().color = buttonReadyColour;
+        stopButton.GetComponent<Image>().color = buttonNotReadyColour;
+
+    }
+
+    private void SaveLists()
+    {
+        string csvString = "times,accelerations\n";
+        for (int i = 0; i < times.Count; i++)
         {
-            csvString += acc.ToString("#0.000") + ",";
+            csvString += times[i].ToString() + "," + accs[i].ToString("#0.000") + "\n";
         }
 
         string folderPath = Path.Combine(Application.persistentDataPath, "accelerometer");
@@ -64,17 +76,13 @@ public class AccelerometerRecorder : MonoBehaviour
         {
             Directory.CreateDirectory(folderPath);
         }
-        string filename = "acc_" + DateTime.Now.ToString("yyMMdd-HHmmss") + ".txt";
+        string filename = "acc_" + DateTime.Now.ToString("yyMMdd-HHmmss") + ".csv";
         File.WriteAllText(Path.Combine(folderPath, filename), csvString);
         Debug.Log(filename + " written to " + folderPath);
-
-        startButton.GetComponent<Image>().color = buttonReadyColour;
-        stopButton.GetComponent<Image>().color = buttonNotReadyColour;
-
     }
 
 
-    public float CalcSmoothness()
+    public static float CalcSmoothness(List<float> accs)
     {
         float avg = 0;
         int count = 0;
