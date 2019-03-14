4a4b689d765af96519ca4a3145c4472c661c52d6
Luke Storry <luke@lukestorry.co.uk>
2019-03-14 23:16:29 +0000

    Added a vibrating countdown timer to acc recorder
    v0.1.3

--- src/Assets/Scripts/AccelerometerRecorder.cs
+++ src/Assets/Scripts/AccelerometerRecorder.cs
@@ -12,6 +12,7 @@ public class AccelerometerRecorder : MonoBehaviour
     private List<DataPoint> data;
     private bool running = false;
     private Color buttonReadyColour, buttonNotReadyColour;
+    private int countdown;
 
     void Start()
     {
@@ -24,23 +25,36 @@ public class AccelerometerRecorder : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (running)
+        if (!running) return;
+
+        if (Timer() < 0)
         {
-            long time = DateTime.Now.Ticks;
-            float acc = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z).magnitude;
-            data.Add(new DataPoint(time, acc));
-            timerText.text = Timer().ToString("#0.0");
+            timerText.text = "starting in " + Mathf.Ceil(-Timer()).ToString();
+            if (countdown > -Timer())
+            {
+                Handheld.Vibrate();
+                countdown -= 1;
+            }
         }
+        else
+        {
+            timerText.text = Timer().ToString("#0.0");
+            float acc = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z).magnitude;
+            data.Add(new DataPoint(DateTime.Now.Ticks, acc));
+        }
+
     }
 
 
     public void StartRecord()
     {
         if (running) return;
+
         running = true;
         Debug.Log("Started");
 
-        startTime = DateTime.Now.Ticks;
+        countdown = 5;
+        startTime = DateTime.Now.Ticks + countdown * 10000000;
         data = new List<DataPoint>();
         startButton.GetComponent<Image>().color = buttonNotReadyColour;
         stopButton.GetComponent<Image>().color = buttonReadyColour;
@@ -52,11 +66,14 @@ public class AccelerometerRecorder : MonoBehaviour
         running = false;
         Debug.Log("Stopped");
 
-        ClimbData climb = new ClimbData(data);
-        timerText.text = climb.InfoText;
+        if (data.Count > 10)
+        {
+            ClimbData climb = new ClimbData(data);
+            timerText.text = climb.InfoText;
 
-        PersistentInfo.climbs.Insert(0, climb);
-        FileHandler.SaveClimb(climb);
+            PersistentInfo.climbs.Insert(0, climb);
+            FileHandler.SaveClimb(climb);
+        }
 
         startButton.GetComponent<Image>().color = buttonReadyColour;
         stopButton.GetComponent<Image>().color = buttonNotReadyColour;
