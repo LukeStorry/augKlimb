13fe69d85b55a890db0acdc06e989d3b1a5afab1
Luke Storry <luke@lukestorry.co.uk>
2019-03-28 22:36:44 +0000

    set up and published to google play

--- src/Assets/Scripts/MainMenu.cs
+++ src/Assets/Scripts/MainMenu.cs
@@ -12,12 +12,12 @@ public class MainMenu : MonoBehaviour
         gameObject.transform.Find("Accelero Button").GetComponent<Button>().onClick.AddListener(delegate { SceneManager.LoadScene("AccRecord"); });
         gameObject.transform.Find("Video Button").GetComponent<Button>().onClick.AddListener(delegate { SceneManager.LoadScene("Video"); });
         gameObject.transform.Find("ViewData Button").GetComponent<Button>().onClick.AddListener(delegate { SceneManager.LoadScene("ViewAllData"); });
-        //gameObject.transform.Find("???").GetComponent<Button>().onClick.AddListener(delegate { SceneManager.LoadScene("???"); });
+        gameObject.transform.Find("Question Button").GetComponent<Button>().onClick.AddListener(delegate { Application.OpenURL("mailto:ls14172@bristol.ac.uk?subject=augKlimb Feedback"); });
 
-        gameObject.transform.Find("Update Button").GetComponent<Button>().onClick.AddListener(delegate { Application.OpenURL("http://bit.ly/akapk"); });
         gameObject.transform.Find("About Button").GetComponent<Button>().onClick.AddListener(delegate { Application.OpenURL("https://lukestorry.co.uk/augKlimb/"); });
         gameObject.transform.Find("Feedback Button").GetComponent<Button>().onClick.AddListener(delegate { Application.OpenURL("https://goo.gl/forms/GGdaJq2hBdIhsOKX2"); });
 
+        gameObject.transform.Find("Version Text").GetComponent<Button>().onClick.AddListener(delegate { Application.OpenURL("http://bit.ly/akapk"); });
         gameObject.transform.Find("Version Text").GetComponent<Text>().text += Application.version;
     }
 
