056d4e205dce62dfab5ac948b2d95bfbec8da527
Luke Storry <luke@lukestorry.co.uk>
2019-03-29 13:18:29 +0000

    re-added in-app video recorder, with sharing and gallery-saving instead of matching

--- src/Assets/Scripts/VideoRecorder.cs
+++ src/Assets/Scripts/VideoRecorder.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.IO;
 
-public class Videos : MonoBehaviour
+public class VideoRecorder : MonoBehaviour
 {
-
     private Text output;
-
+    private GameObject shareButton;
     void Start()
     {
         output = gameObject.transform.Find("Text").GetComponent<Text>();
+
         gameObject.transform.Find("Record Button").GetComponent<Button>().onClick.AddListener(RecordVideo);
-        //RecordVideo();
+
+        shareButton = gameObject.transform.Find("Share Button").gameObject;
+        shareButton.SetActive(false);
     }
 
     void Update()
@@ -27,33 +29,36 @@ public class Videos : MonoBehaviour
     // Records a video, using https://github.com/yasirkula/UnityNativeCamera
     private void RecordVideo()
     {
+        Debug.Log(FileHandler.vidsFolder);
         DateTime vidTime = DateTime.Now; // TODO edit to take time from video recorder, as this doesnt start the recrod!
-
 
         if (NativeCamera.IsCameraBusy())
             output.text = ("Camera Busy");
 
-        NativeCamera.Permission permission = NativeCamera.RecordVideo(HandleVideo, NativeCamera.Quality.Low);
+        NativeCamera.Permission permission = NativeCamera.RecordVideo(HandleVideo); //, NativeCamera.Quality.Low
 
         Debug.Log("Permission result: " + permission);
     }
 
-    private void HandleVideo(string path)
+    private void HandleVideo(string tempPath)
     {
-        DateTime vidTime = DateTime.Now.Subtract(new TimeSpan(0, 0, 2, (int)NativeCamera.GetVideoProperties(path).duration));
-        Debug.Log("Video time: " + vidTime + " path: " + path);
-
-        if (path != null)
-        {
-            string filepath = FileHandler.CopyVideo(path, vidTime);
-            output.text = "Video Recorded: " + Path.GetFileName(filepath);
-        }
-        else
+        if (tempPath == null || tempPath == "")
         {
             output.text = "Recording failed";
+            StartCoroutine(ResetText());
+            return;
         }
 
-        StartCoroutine(ResetText());
+        string filename = DateTime.Now.ToString("yyyyMMdd_HHmmss") + Path.GetExtension(tempPath);
+
+        NativeGallery.SaveVideoToGallery(tempPath, "augKlimb", filename);
+        output.text = "Video Recorded: " + filename;
+
+        shareButton.SetActive(true);
+        shareButton.GetComponent<Button>().onClick.AddListener(delegate
+        {
+            new NativeShare().AddFile(tempPath).Share();
+        });
     }
 
     private IEnumerator ResetText()
