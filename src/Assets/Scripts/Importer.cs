59b4013c8ddac879a17f674323facd887a1d26d0
Luke Storry <luke@lukestorry.co.uk>
2019-04-03 00:59:02 +0100

    moved to different filebrowser

--- src/Assets/Scripts/Importer.cs
+++ src/Assets/Scripts/Importer.cs
@@ -1,79 +1,73 @@
 ﻿using System;
-using System.IO;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using SimpleFileBrowser;
 
-
-namespace GracesGames.SimpleFileBrowser.Scripts
+public class Importer : MonoBehaviour
 {
+    public Button importButton;
+    public bool text;
+    public bool video;
+    public GameObject fileBrowserPrefab, confirmationBoxPrefab;
 
-    public class Importer : MonoBehaviour
+    void Start()
     {
-        public Button importButton;
-        public bool text;
-        public bool video;
-        public GameObject fileBrowserPrefab, confirmationBoxPrefab;
+        if (!(text ^ video)) throw new Exception("Either text OR video importer must be selected");
 
-        void Start()
-        {
-            if (!(text ^ video)) throw new Exception("Either text OR video importer must be selected");
+        if (text)
+            importButton.onClick.AddListener(LaunchFileBrowser);
 
-            if (text)
-                importButton.onClick.AddListener(LaunchFileBrowser);
+        if (video)
+            importButton.onClick.AddListener(LaunchGalleryBrowser);
+    }
 
-            if (video)
-                importButton.onClick.AddListener(LaunchGalleryBrowser);
-        }
+    // launches a platform-specific Gallery browser and returns the path of the selected file
+    private void LaunchGalleryBrowser()
+    {
+        NativeGallery.GetVideoFromGallery(TryImportFile, "Pick a Video", "video/*");
+    }
 
-        // launches a platform-specific Gallery browser and returns the path of the selected file
-        private void LaunchGalleryBrowser()
-        {
-            NativeGallery.GetVideoFromGallery(TryImportFile, "Pick a Video", "video/*");
-        }
-
-        // launches a platform-specific file browser and returns the path of the selected file
-        private void LaunchFileBrowser()
-        {
-            GameObject fileBrowserObject = Instantiate(fileBrowserPrefab, transform);
-            FileBrowser fileBrowserScript = fileBrowserObject.GetComponent<FileBrowser>();
-            fileBrowserScript.SetupFileBrowser(ViewMode.Portrait);
-            fileBrowserScript.OpenFilePanel(new string[] { "txt" });
-            fileBrowserScript.OnFileSelect += TryImportFile;  // subscribes to event (calls using path) 
-        }
-
-        // selects and parses an external file, then saves to data folder (cached). Displays exceptions in the textbox of attached object.
-        private void TryImportFile(string path)
-        {
-            try
-            {
-                if (video)
-                    FileHandler.ImportVideo(path);
-                else if (text)
-                    FileHandler.LoadClimb(path, copy: true);
-
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
-            catch (Exception e)
-            {
-                Debug.Log(e.Message);
-                DisplayText(e.Message);
-            }
-        }
-
-
-        private void DisplayText(string text)
-        {
-            GameObject confirmationDialog = Instantiate(confirmationBoxPrefab, gameObject.transform);
-            confirmationDialog.transform.Find("Message").gameObject.GetComponent<Text>().text = text;
-            confirmationDialog.transform.Find("No Button").gameObject.GetComponent<Text>().text = "ok";
-            confirmationDialog.transform.Find("No Button").gameObject.GetComponent<Button>().onClick.AddListener(delegate
-            {
-                Destroy(confirmationDialog);
-            });
-            confirmationDialog.transform.Find("Yes Button").gameObject.SetActive(false);
-        }
+    // launches a platform-specific file browser and returns the path of the selected file
+    private void LaunchFileBrowser()
+    {
+        FileBrowser.SetFilters(false, new FileBrowser.Filter("Climb Files", ".txt"));
+        FileBrowser.SetDefaultFilter(".txt");
+        FileBrowser.ShowLoadDialog(TryImportFile, null, false, "Load Climb File", "Select");
 
     }
+
+    // selects and parses an external file, then saves to data folder (cached). Displays exceptions in the textbox of attached object.
+    private void TryImportFile(string path)
+    {
+        try
+        {
+            if (video)
+                FileHandler.ImportVideo(path);
+            else if (text)
+                FileHandler.LoadClimb(path, copy: true);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            DisplayText(e.Message);
+        }
+    }
+
+
+    private void DisplayText(string text)
+    {
+        GameObject confirmationDialog = Instantiate(confirmationBoxPrefab, gameObject.transform);
+        confirmationDialog.transform.Find("Message").gameObject.GetComponent<Text>().text = text;
+        confirmationDialog.transform.Find("No Button").gameObject.GetComponent<Text>().text = "ok";
+        confirmationDialog.transform.Find("No Button").gameObject.GetComponent<Button>().onClick.AddListener(delegate
+        {
+            Destroy(confirmationDialog);
+        });
+        confirmationDialog.transform.Find("Yes Button").gameObject.SetActive(false);
+    }
+
+
 }
